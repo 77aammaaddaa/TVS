3529e777edb1676ae298cddd6a425ff631b525d3
Mr X <52516567+77aammaaddaa@users.noreply.github.com>
2026-03-15 00:13:16 +0200

    Update feedback.js

--- ecofine/feedback.js
+++ ecofine/feedback.js
@@ -1,8 +1,1 @@
-<div className="mt-10 text-center border-t border-white/10 pt-4">
-                    <p className="text-[9px] text-slate-500 font-bold">Powered by Techno Vision Solutions © 2026</p>
-                    <p className="text-[8px] text-slate-600 mt-1">Suez, Egypt | X-Holding</p>
-                </div>
-            </div>
-        </div>
-    );
-};
+
